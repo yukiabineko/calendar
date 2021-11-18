968f91870b7571ac07e07d635f99fa8260e930cf
植松勇貴 <uematsuyuuki@uematsuyuukinoMacBook-Pro.local>
2021-11-18 22:28:35 +0900

    トップモーダル追加

--- js/top.js
+++ js/top.js
@@ -12,3 +12,33 @@ function closeDrower(){
     let checkbox = document.getElementById('hm-menu');
     checkbox.checked = false;
 }
+/**
+ * モーダルを開く
+ */
+function openTopModal(){
+  let checkbox = document.getElementById('hm-menu');
+  checkbox.checked = false;
+
+  let menus = document.getElementById('c_lb');
+  menus.style.display = 'none';
+
+  let menuTitle = document.getElementById('menu-title');
+  menuTitle.style.display = 'none';
+
+  let modal = document.getElementById('top-modal');
+  modal.style.display ='block';
+ 
+}
+/**
+ * モーダルを閉じる
+ */
+function closeTopModal(){
+  let menus = document.getElementById('c_lb');
+  menus.style.display = 'flex';
+
+  let menuTitle = document.getElementById('menu-title');
+  menuTitle.style.display = 'block';
+
+  let modal = document.getElementById('top-modal');
+  modal.style.display ='none';
+}
--- view/top/index_view.php
+++ view/top/index_view.php
@@ -1,4 +1,4 @@
-<div class="main-visual">
+<div class="main-visual" id="main-visual">
     <img src="../image/main.jpg" alt="メインビジュアル">
     <div class="visual-content">
         <h3>スケジュール管理アプリ</h3>
@@ -11,13 +11,31 @@
             <p style="margin-top:12px;font-size:10px;color:white; ">機能のご利用はログインしてご利用ください。</p>
             <a href="/calendar/session/new">ログイン</a>
          <?php endif; ?>
-         <!-- モーダル -->
     </div>
+    <!-- モーダル -->
+    <div id="top-modal">
+      <div class="bg"></div>
+      <div class="top-modal-content" id="top-content">
+        <div class="top-modal-close-button">
+          <button onclick="closeTopModal()">閉じる</button>
+        </div>
+
+        <div class="top-modal-heder">
+          <h3 id="top-modal-title">【本日の作業一覧】</h3>
+        </div>
+
+        <div class="content-body">
+         
+        </div>
+      </div>
+    </div>
+   <!--/モーダル -->
+
     <?php if(isset($current_user)) : ?>
       <div class="hamburger">
         <input type="checkbox" id="hm-menu">
         <label for="hm-menu" class="lb" id="c_lb"><span></span></label>
-        <p style="color:white;margin-top:-2%;">menu</p>
+        <p style="color:white;margin-top:-2%;" id="menu-title">menu</p>
         <div class="drower" id="drower">
             <div class="close">
                 <button id="closebutton" onclick="closeDrower()">✖️</button>
@@ -25,15 +43,23 @@
             <div class="drower-header">【メニュー】</div>
             <dl class="drower-dl">
               <dt>スケジュール状況</dt>
-              <dd>本日の作業:【<span style="color:red;"><?= count($today_data) ?></span>】件</dd>
-              <dd>今週の作業:【<span style="color:red;"><?= count($weeks_data) ?></span>】件</dd>
-              <dd>今月の作業:【<span style="color:red;"><?= count($months_data) ?></span>】件</dd>
-              <dd style="background:coral;color:mediumblue">本日の未完了の作業:【<span style="color:navajowhite;"><?= count($today_incompletes) ?></span>】件</dd>
-              <dd style="background:coral;color:mediumblue">今週の未完了の作業:【<span style="color:navajowhite;"><?= count($weekly_incompletes) ?></span>】件</dd>
-              <dd style="background:coral;color:mediumblue">今月の未完了の作業: 【<span style="color:navajowhite;"><?= count($monthly_incomletes) ?></span>】件</dd>
+              <dd>
+                <button onclick="openTopModal()">
+                  本日の作業:【<span style="color:red;"><?= count($today_data) ?></span>】件
+                </button>
+              </dd>
+
+              <dd><button>今週の作業:【<span style="color:red;"><?= count($weeks_data) ?></span>】件</button></dd>
+              <dd><button>今月の作業:【<span style="color:red;"><?= count($months_data) ?></span>】件</button></dd>
+              <dd style="background:coral;color:mediumblue"><button>本日の未完了の作業:【<span style="color:navajowhite;"><?= count($today_incompletes) ?></span>】件</button></dd>
+              <dd style="background:coral;color:mediumblue"><button>今週の未完了の作業:【<span style="color:navajowhite;"><?= count($weekly_incompletes) ?></span>】件</button></dd>
+              <dd style="background:coral;color:mediumblue"><button>今月の未完了の作業: 【<span style="color:navajowhite;"><?= count($monthly_incomletes) ?></span>】件</button></dd>
             </dl>
         </div>
       </div>
     <?php endif; ?>
+     
 </div>
+
+
 
